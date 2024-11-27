e1b97dd111449b2807bd898ddd8a227cace6056b
josebeco <jose.p.sambatti@gmail.com>
2024-11-27 18:16:29 -0300

    a

--- lib/HandlerSprites.cpp
+++ lib/HandlerSprites.cpp
@@ -3,9 +3,73 @@
 #include "RGB.h"
 using namespace std;
 
-void readSprites(string fileName, struct RGB sprites[]){
-    string size;
-    ifstream reader(fileName);
-    getline(reader, size);
+const struct RGB BLACK = {255, 255, 255};
+const struct RGB NONE = {-1, -1, -1};
 
+bool isSeparator(char c)
+{
+    return c == ' ' || c == '\n' || c == '\0';
+}
+
+bool isEmpty(string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (!isSeparator(str[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void readSprites(string fileName, struct RGB sprites[])
+{
+    string str, aux;
+    int j, height, witdh, qtd;
+    ifstream reader(fileName);
+    getline(reader, str);
+
+    aux = "";
+    for (int i = 0; i < str.length() && !isSeparator(str[i]); i++)
+    {
+        aux += str[i];
+        j = i;
+    }
+
+    height = stoi(aux);
+
+    aux = "";
+    while (j < str.length() && !isSeparator(str[j]))
+    {
+        aux += str[j];
+        j++;
+    }
+
+    witdh = stoi(aux);
+
+    aux = "";
+    while (j < str.length() && !isSeparator(str[j]))
+    {
+        aux += str[j];
+        j++;
+    }
+
+    qtd = stoi(aux);
+
+    sprites = new struct RGB[(qtd * witdh) + height];
+    
+    for (int i = 0; getline(reader, str); i++)
+    {
+        if (isEmpty(str))
+        {
+            i--;
+            continue;
+        }
+
+        for (int j = 0; j < witdh; j++)
+        {
+            sprites[(i * witdh) + j] = (str[j] == '.') ? NONE : BLACK;
+        }
+    }
 }
