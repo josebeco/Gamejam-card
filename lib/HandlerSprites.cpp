e460729bf6a9e29a209494f8bccab841db87c4f6
josebeco <jose.p.sambatti@gmail.com>
2024-11-27 11:37:53 -0300

    just to save

---
+++ lib/HandlerSprites.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <fstream>
+#include "RGB.h"
+using namespace std;
+
+void readSprites(string fileName, struct RGB sprites[]){
+    string size;
+    ifstream reader(fileName);
+    getline(reader, size);
+    
+}
---
+++ lib/RGB.h
@@ -0,0 +1,9 @@
+#ifndef RGBHEADER
+#define RGBHEADER
+
+struct RGB
+{
+    int r, g, b;
+} RGB;
+
+#endif
