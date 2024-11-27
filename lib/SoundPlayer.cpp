8b1c7b8676924df1a50b35af51ef97111f4d90c7
josebeco <zezinhocarro@gmail.com>
2024-11-27 17:48:07 -0300

    a

--- lib/SoundPlayer.cpp
+++ lib/SoundPlayer.cpp
@@ -1,15 +1,24 @@
 #include <SDL2/SDL.h>
+#include <string>
+using namespace std;
 
-
-int main()
-{
+void init(){
     SDL_Init(SDL_INIT_AUDIO);
+}
+
+void close(){
+     SDL_Quit();
+}
+
+void playSound(string fileName)
+{
 
     SDL_AudioSpec wav_spec;
     Uint32 wav_length;
     Uint8 *wav_buffer;
+    const char * file = fileName.c_str();
 
-    if (SDL_LoadWAV("ultima.wav", &wav_spec, &wav_buffer, &wav_length))
+    if (SDL_LoadWAV(file, &wav_spec, &wav_buffer, &wav_length))
     {
         SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0,  &wav_spec, NULL, 0);
 
@@ -18,8 +27,9 @@ int main()
             int success = SDL_QueueAudio(deviceId, wav_buffer, wav_length);
 
             SDL_PauseAudioDevice(deviceId, 0);
-             SDL_ShowSimpleMessageBox(0, "A", "AudiAAAAAAAAAAAAa", NULL);
+            
             SDL_CloseAudioDevice(deviceId);
+        
         }
         else
         SDL_ShowSimpleMessageBox(0, "Error", "Audio driver failed to initialize", NULL);
@@ -29,7 +39,4 @@ int main()
     else
         SDL_ShowSimpleMessageBox(0, "Error", "wav failed to load", NULL);
 
-    SDL_Quit();
-
-    return 0;
 }
