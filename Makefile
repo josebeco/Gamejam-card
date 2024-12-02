17842a06ecb7c9b784186f831f2573ec23c1f03f
josebeco <jose.p.sambatti@gmail.com>
2024-12-01 21:33:55 -0300

    a

--- Makefile
+++ Makefile
@@ -6,6 +6,12 @@ build:
 run: build
 	./game
 
+static:
+	g++ -o game Main.cpp code/*.cpp lib/*.cpp simulador/*.cpp -lglut -lGLU -lGL -lSDL2 -static
+
+win:
+	x86_64-w64-mingw32-g++ -o game Main.cpp code/*.cpp lib/*.cpp simulador/*.cpp -lglut -lGLU -lGL -lSDL2 -static
+
 git:
 	git config --global user.name josebeco
 	git config --global user.email jose.p.sambatti@gmail.com
--- simulador/simulatorH/Player.h
+++ simulador/simulatorH/Player.h
@@ -1,17 +1,17 @@
 #include "Carta.h"
 
 #ifndef PLAYERHEADER
-
+#define AMOUNT_CARDS 20
 #define PLAYERHEADER
 struct Player{
     int adr = 50;
-    struct Carta baralho[25];
-    int indBaralho = 19;
-    struct Carta mao[25];
+    struct Carta baralho[AMOUNT_CARDS + 5];
+    int indBaralho = AMOUNT_CARDS - 1;
+    struct Carta mao[AMOUNT_CARDS + 5];
     int maoLength = 0;
     struct Carta emJogo[5];
     bool livreEmJogo[5];
-    struct Carta descarte[25];
+    struct Carta descarte[AMOUNT_CARDS + 5];
     int descarteLength = 0;
 };
 #endif
